2ccbabb236fd7ac88a3dff578b29ef1566a363c8
JulianNicolai <juliannicolai123@gmail.com>
2021-03-19 22:38:28 -0400

    Added JUnit test infrastructure; moved classes into package 'store'

---
+++ src/com/company/store/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // main entry point is using StoreView.main()
+    }
+}
---
+++ src/storetest/InventoryTest.java
@@ -0,0 +1,2 @@
+package storetest;public class InventoryTest {
+}
